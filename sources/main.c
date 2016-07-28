7723af5839b0a2de134430610a37c34020a96e91
Alexandre LEGENT <alexandrelegent@gmail.com>
2016-07-28 11:58:56 +0200

    Adding libraries + CMakeLists.txt

---
+++ sources/main.c
@@ -0,0 +1,17 @@
+//
+//           :::      ::::::::
+//         :+:      :+:    :+:     main.c
+//       +:+ +:+         +:+
+//     +#+  +:+       +#+          By: Alexandre LEGENT <alegent@student.42.fr>
+//   +#+#+#+#+#+   +#+
+//        #+#    #+#
+//       ###   ###########.fr      Created: 28/07/2016 11:45 by alegent
+//
+
+#include "ft_printf.h"
+
+int					main(void)
+{
+	ft_printf("Hello World\n");
+	return (42);
+}
