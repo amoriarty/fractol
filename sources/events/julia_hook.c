d05c3221cb5dd55bce3482abf030214e2876e23d
Alexandre LEGENT <alexandrelegent@gmail.com>
2016-09-08 16:05:05 +0200

    Petit correction julia hook (juste pour me rassurer !)

--- sources/events/julia_hook.c
+++ sources/events/julia_hook.c
@@ -16,13 +16,13 @@ int					julia_hook(int x, int y, t_mlx *mlx)
 {
 	if (x > 0 && x < WIN_X && y > 0 && y < WIN_Y)
 	{
-		if (x < WIN_X / 2 && y < WIN_Y / 2)
+		if (x < WIN_X / 2 && y < WIN_Y)
 			mlx->fractal->cplx->c_r += 0.05;
-		if (x > WIN_X / 2 && y < WIN_Y / 2)
+		if (x > WIN_X / 2 && y < WIN_Y)
 			mlx->fractal->cplx->c_r -= 0.05;
-		if (x < WIN_X / 2 && y > WIN_Y / 2)
+		if (x < WIN_X / 2 && y > WIN_Y)
 			mlx->fractal->cplx->c_i += 0.05;
-		if (x > WIN_X / 2 && y > WIN_Y / 2)
+		if (x > WIN_X / 2 && y > WIN_Y)
 			mlx->fractal->cplx->c_i -= 0.05;
 	}
 	fractol(mlx, mlx->fractal->type);
